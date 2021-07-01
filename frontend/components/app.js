9f4d44e0b43e02b23e38dc42e5cec0ce7c01a8cc
Taylor Musolf <tcmusolf@gmail.com>
2021-07-01 12:47:34 -0700

    First commit

---
+++ frontend/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
+
+const App = () => (
+  <div className='big-div'>
+      <div className= 'main-content'>
+        //switch and routes
+      </div>
+  </div>
+);
+
+export default App;
